f0e4701ac14bc83a6707b337b5441a82da5cce97
Guest User <Guest@Epicodus-4C.local>
2018-01-19 10:14:47 -0800

    set up basic loop of animals list

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,25 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Zoo is Ready!</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li *ngFor="let thisAnimal of animals">{{thisAnimal.species}} is named "{{thisAnimal.name}}"</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Current Beasts';
 
+  animals: Animal[] = [
+    new Animal("shrimp", "Seu"),
+    new Animal("shark", "Sano"),
+    new Animal("chickadee", "Chickadee"),
+  ];
+
+}
+
+export class Animal {
+  constructor(public species: string, public name: string) { }
 }
